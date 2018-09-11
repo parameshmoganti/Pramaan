ab2b2f2feef55319241b8043857c204559affba6
Nagavenkata Paramesh Moganti <Nagavenkata.Moganti@APTROIDINDIA.COM>
2018-09-11 19:20:51 +0530

    modified

--- send_onetraport_test.php
+++ send_onetraport_test.php
@@ -3,13 +3,21 @@ require_once('SDK-PHP/src/APIAutoloader.php');
 
 $client = new OntraportAPI\Ontraport("2_186126_xDklkgrgi","zhu5Ezge5Cs4Sv7");
 $requestParams = array(
-"firstname" => "tim",
-   "lastname" => "lincecum",
-   "email" => "t.lincecum@test.com",
-   "contact_cat" => "*/*55*/*",
-   "updateSequence" => "*/*97*/*"
+	"firstname" => "tim",
+  "lastname" => "lincecum",
+   "email" => "lin@test.com",                     
 );
 
+
 $response = $client->contact()->saveOrUpdate($requestParams);
-print_r($response);
+#$response = $client->contact()->retrieveCollectionInfo($requestParams);
+
+$json_output = json_decode($response); 
+if(array_key_exists("attrs",$json_output->data))
+{
+	echo "duiplicate";
+	echo $json_output->data->attrs->id;
+}
+else{
+echo $json_output->data->id;}
 ?>
